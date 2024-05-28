76f764ec72cd3bdcd6e937ffc4a0dad07307d2bb
Mannus <tuanha.211101@gmail.com>
2024-05-28 14:03:22 +0700

    a update

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,51 +1,55 @@
-import axios from 'axios';
-import queryString from 'query-string';
-import firebase from "firebase"
+import axios from "axios";
+import queryString from "query-string";
+import firebase from "firebase";
 
 const getFirebasetoken = async () => {
-    const currenUser = firebase.auth().currentUser;
-    if (currenUser) {
-        return currenUser.getIdToken();
-    }
-    const hasRememberAccount = localStorage.getItem("token");
-    if (!hasRememberAccount) return null;
+  const currenUser = firebase.auth().currentUser;
+  if (currenUser) {
+    return currenUser.getIdToken();
+  }
+  const hasRememberAccount = localStorage.getItem("token");
+  if (!hasRememberAccount) return null;
 
-    return new Promise((resolve, reject) => {
-        const unregisterAuthObserver = firebase.auth().onAuthStateChanged(async (user) => {
-            if (!user) {
-                reject(null)
-            }
-            const token = await user.getIdToken();
-            console.log("axios token" + token);
-            resolve(token);
-            unregisterAuthObserver();
+  return new Promise((resolve, reject) => {
+    const unregisterAuthObserver = firebase
+      .auth()
+      .onAuthStateChanged(async (user) => {
+        if (!user) {
+          reject(null);
         }
-        )
-    })
-}
+        const token = await user.getIdToken();
+        console.log("axios token" + token);
+        resolve(token);
+        unregisterAuthObserver();
+      });
+  });
+};
 const axiosClient = axios.create({
-    baseURL: `http://localhost:666`,
-    headers: {
-        'content-type': "application/json",
-    },
-    paramsSerializer: params => queryString.stringify(params),
+  baseURL: `http://0.tcp.ap.ngrok.io:11172`,
+  headers: {
+    "content-type": "application/json",
+  },
+  paramsSerializer: (params) => queryString.stringify(params),
 });
 axiosClient.interceptors.request.use(async (config) => {
-    // const token = await getFirebasetoken();
-    const token = localStorage.getItem("token");
-    if (token) {
-        config.headers.authorization = `Bearer ${token}`
-        //console.log(token);
-    }
-    return config;
-})
-axiosClient.interceptors.response.use((response) => {
+  // const token = await getFirebasetoken();
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.authorization = `Bearer ${token}`;
+    //console.log(token);
+  }
+  return config;
+});
+axiosClient.interceptors.response.use(
+  (response) => {
     if (response && response.data) {
-        return response.data;
+      return response.data;
     }
     return response;
-}, (error) => {
+  },
+  (error) => {
     // Handle errors
     throw error;
-});
+  }
+);
 export default axiosClient;
